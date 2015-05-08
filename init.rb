ad543fea18f2102cfc0de4d2f43653cd52e7ec0f
GJ Roelofs <info@codepoke.net>
2015-05-08 14:36:20 +0200

    Added option to specify the transpose for the header section.

--- init.rb
+++ init.rb
@@ -10,9 +10,15 @@ Redmine::Plugin.register :redmine_wiki_macro_ext do
   author_url 'gj.roelofs@codepoke.net'
 
 	Redmine::WikiFormatting::Macros.register do
-		desc "Provide a summary of the parameter page. \nUsage:\n<pre>{{summary(<page>)}}</pre>"
+		desc "Provide a summary of the parameter page. \n"+
+			"Usage: <pre>\n" +
+			"{{summary(<page>)}} \n" +
+			"{{summary(<page>, header=N}} to adjust the header number by N \n"
+			"</pre>"
 
 		macro :summary do |obj, args|
+
+			args, options = extract_macro_options(args, :header)
 
 			page = nil
 			if args.size == 1
@@ -21,15 +27,21 @@ Redmine::Plugin.register :redmine_wiki_macro_ext do
 				raise 'Page not found' if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
 			end
 
+			n = options[:header]
+                        raise 'Invalid header parameter' unless n.nil? || n.match(/\d+$/)
+			n = n.to_i
+
 			# Match to the text between the first and second header. (nclude header)
 			@included_wiki_pages ||= []
-	        raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title)
-	        @included_wiki_pages << page.title
-                #content = page.content[/^(h[1-6]\.*)/sm]
-		#out = page.content.text
-	        out = textilizable(page.content.text[/(h[1-6]\..*?)(^h[1-6]\.|\Z)/sm, 1], :attachments => page.attachments, :headings => false)
-	        @included_wiki_pages.pop
-	        out
+
+		        raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title)
+		        @included_wiki_pages << page.title
+               	 	#content = page.content[/^(h[1-6]\.*)/sm]
+			text = page.content.text[/(h[1-6]\..*?)(^h[1-6]\.|\Z)/sm, 1]
+			text = text.gsub(/h([1-6])\./) {"h" + ([6,[1, $1.to_i + n].max].min).to_s + "."}
+	 	      	out = textilizable(text, :attachments => page.attachments, :headings => false)
+	        	@included_wiki_pages.pop
+	        	out
 
 		end 
 	end
